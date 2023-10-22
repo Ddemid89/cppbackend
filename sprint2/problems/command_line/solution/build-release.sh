40ef54dfc27f197d8c8c0c789f29bf08b2f565d9
Алексей Демидов <ddemid1989@yandex.ru>
2023-10-22 13:32:05 +0300

    remove final solution

--- sprint2/problems/command_line/solution/build-release.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-rm -rf ./build-release
-mkdir build-release
-cd build-release
-
-conan install .. --build=missing -s build_type=Release -s compiler.libcxx=libstdc++11
-cmake .. -DCMAKE_BUILD_TYPE=Release
-cmake --build .
